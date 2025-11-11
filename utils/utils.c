6802b173237d52b2276b338afc4c049e25ee4900
ettisafxrup <ettisafrup@gmail.com>
2025-11-11 11:13:15 +0600

    removed progressBar function

--- utils/utils.c
+++ utils/utils.c
@@ -13,18 +13,3 @@ void pauseScreen()
     printf("Press Enter to Continue:");
     getchar();
 }
-
-void showProgressBar(int percent)
-{
-    int barWidth = 50;
-    int pos = (barWidth * percent) / 100;
-    printf("[");
-    for (int i = 0; i < barWidth; ++i)
-    {
-        if (i < pos)
-            printf("█");
-        else
-            printf("░");
-    }
-    printf("] %d%%\n", percent);
-}
